a3e600d5ad6bff2440311ead3838857f5504d095
= <abdeluached@gmail.com>
2022-12-07 16:24:54 +0100

    feat: home page

--- example/lib/data/home_json.dart
+++ example/lib/data/home_json.dart
@@ -1,4 +1,63 @@
-Map<String, dynamic> homeJson = {
+final List items = [
+  {
+    'id': '1',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1628277613967-6abca504d0ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'title': 'Flutter',
+    'subtitle': 'Dart',
+  },
+  {
+    'id': '2',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1639507986194-48ef61205b61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
+    'title': 'Django',
+    'subtitle': 'Python',
+  },
+  {
+    'id': '3',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'title': 'ReactNative',
+    'subtitle': 'JavaScript',
+  },
+  {
+    'id': '4',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'title': 'Laravel',
+    'subtitle': 'Php',
+  },
+  {
+    'id': '5',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    'title': 'Flask',
+    'subtitle': 'Python',
+  },
+  {
+    'id': '6',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1569693799105-4eb645d89aea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'title': 'JetPack Compose',
+    'subtitle': 'Kotlin',
+  },
+  {
+    'id': '7',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1658597610657-463f08214b98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1522&q=80',
+    'title': 'Unity',
+    'subtitle': 'C#',
+  },
+  {
+    'id': '8',
+    'imageUrl':
+        'https://images.unsplash.com/photo-1597029449090-ce091df67378?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'title': 'Angular',
+    'subtitle': 'JavaScript',
+  },
+];
+
+Map<String, dynamic> homeJsonData = {
   'type': 'Scaffold',
   'appBar': {
     'type': 'AppBar',
@@ -17,62 +76,21 @@ Map<String, dynamic> homeJson = {
       },
       'child': {
         'type': 'Column',
-        'children': [
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1628277613967-6abca504d0ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-            title: 'Flutter',
-            subtitle: 'Dart',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1639507986194-48ef61205b61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
-            title: 'Django',
-            subtitle: 'Python',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1623282033815-40b05d96c903?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-            title: 'ReactNative',
-            subtitle: 'JavaScript',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-            title: 'Laravel',
-            subtitle: 'Php',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1543966888-7c1dc482a810?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
-            title: 'Flask',
-            subtitle: 'Python',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1569693799105-4eb645d89aea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-            title: 'JetPack Compose',
-            subtitle: 'Kotlin',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1658597610657-463f08214b98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1522&q=80',
-            title: 'Unity',
-            subtitle: 'C#',
-          ),
-          listTile(
-            imageUrl:
-                'https://images.unsplash.com/photo-1597029449090-ce091df67378?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-            title: 'Angular',
-            subtitle: 'JavaScript',
-          ),
-        ],
+        'children': items.map((item) {
+          return listTileJson(
+            id: item['id'],
+            imageUrl: item['imageUrl'],
+            title: item['title'],
+            subtitle: item['subtitle'],
+          );
+        }).toList(),
       },
     },
   },
 };
 
-Map<String, dynamic> listTile({
+Map<String, dynamic> listTileJson({
+  required String id,
   required String imageUrl,
   required String title,
   required String subtitle,
--- example/lib/ui/home/home.dart
+++ example/lib/ui/home/home.dart
@@ -7,6 +7,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return UIXAttributes.fromJson(homeJson).build();
+    return UIXAttributes.fromJson(homeJsonData).build();
   }
 }
