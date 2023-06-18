fdd54c584b7ac5e8a067ee57cdea702cbc239715
pc <abdeluached@gmail.com>
2023-06-18 19:59:12 +0100

    feat: improve and clean code

--- lib/src/common/converters/alignment.dart
+++ lib/src/common/converters/alignment.dart
@@ -1,35 +1,38 @@
 part of 'converters.dart';
 
-class UIXAlignmentGeometryConverter implements JsonConverter<AlignmentGeometry?, String?> {
+class UIXAlignmentGeometryConverter
+    implements JsonConverter<AlignmentGeometry?, String?> {
   const UIXAlignmentGeometryConverter();
 
   @override
   AlignmentGeometry? fromJson(String? json) {
-    return <String, AlignmentGeometry>{
-      'topStart': AlignmentDirectional.topStart,
-      'topCenter': AlignmentDirectional.topCenter,
-      'topEnd': AlignmentDirectional.topEnd,
-      'bottomStart': AlignmentDirectional.bottomStart,
-      'bottomCenter': AlignmentDirectional.bottomCenter,
-      'bottomEnd': AlignmentDirectional.bottomEnd,
-      'centerStart': AlignmentDirectional.centerStart,
-      'center': AlignmentDirectional.center,
-      'centerEnd': AlignmentDirectional.centerEnd,
-    }[json];
+    return switch (json) {
+      'topStart' => AlignmentDirectional.topStart,
+      'topCenter' => AlignmentDirectional.topCenter,
+      'topEnd' => AlignmentDirectional.topEnd,
+      'bottomStart' => AlignmentDirectional.bottomStart,
+      'bottomCenter' => AlignmentDirectional.bottomCenter,
+      'bottomEnd' => AlignmentDirectional.bottomEnd,
+      'centerStart' => AlignmentDirectional.centerStart,
+      'center' => AlignmentDirectional.center,
+      'centerEnd' => AlignmentDirectional.centerEnd,
+      _ => null,
+    };
   }
 
   @override
   String? toJson(AlignmentGeometry? data) {
-    return <AlignmentGeometry, String>{
-      AlignmentDirectional.topStart: 'topStart',
-      AlignmentDirectional.topCenter: 'topCenter',
-      AlignmentDirectional.topEnd: 'topEnd',
-      AlignmentDirectional.bottomStart: 'bottomStart',
-      AlignmentDirectional.bottomCenter: 'bottomCenter',
-      AlignmentDirectional.bottomEnd: 'bottomEnd',
-      AlignmentDirectional.centerStart: 'centerStart',
-      AlignmentDirectional.center: 'center',
-      AlignmentDirectional.centerEnd: 'centerEnd',
-    }[data];
+    return switch (data) {
+      AlignmentDirectional.topStart => 'topStart',
+      AlignmentDirectional.topCenter => 'topCenter',
+      AlignmentDirectional.topEnd => 'topEnd',
+      AlignmentDirectional.bottomStart => 'bottomStart',
+      AlignmentDirectional.bottomCenter => 'bottomCenter',
+      AlignmentDirectional.bottomEnd => 'bottomEnd',
+      AlignmentDirectional.centerStart => 'centerStart',
+      AlignmentDirectional.center => 'center',
+      AlignmentDirectional.centerEnd => 'centerEnd',
+      _ => null,
+    };
   }
 }
